aaf97944fab2c0b6ae1e672eebeaa5dd067ad7b3
Ubuntu <azureuser@tutor.u401mzvurnce1fdbd4i2lfuzqd.rx.internal.cloudapp.net>
2024-05-23 12:49:14 +0000

    update

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,14 +1,25 @@
-import { getAuthenticatedUser, getLoginRedirectUrl, getLogoutRedirectUrl } from '@edx/frontend-platform/auth';
+import { getAuthenticatedUser, getLoginRedirectUrl } from '@edx/frontend-platform/auth';
 import { Button, Container } from '@edx/paragon';
-import { Redirect } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
+const EG_QUERY = gql`
+    query MyQuery {
+        auth_user(where: {is_staff: {_eq: 1}}) {
+            email
+            id
+            is_staff
+            is_active
+            is_superuser
+        }
+    }
+`;
 
 const ExamplePage = () => {
 
     const authenticatedUser = getAuthenticatedUser();
     const redirectUrl = getLoginRedirectUrl(window.location.origin + '/authenticated');
-    const logoutUrl = getLogoutRedirectUrl(window.location.origin + '/authenticated');
 
-    console.log(redirectUrl);
+    const { loading, error, data } = useQuery(EG_QUERY);
 
     if (!authenticatedUser) {
         return (
@@ -30,6 +41,16 @@ const ExamplePage = () => {
                         <p>Authenticated User:</p>
                         <pre>{JSON.stringify(authenticatedUser, null, 2)}</pre>
                     </div>
+
+                    <div>
+                        {data && data.auth_user.length > 0 && (
+                            <div>
+                                <p>Example Query:</p>
+                                <pre>{JSON.stringify(data.auth_user, null, 2)}</pre>
+                            </div>
+                        )}
+                    </div>
+
                     <Button href={process.env.LOGOUT_URL + `?next=${window.location.origin}/authenticated`} variant="primary">Logout</Button>
                 </Container>
             </main>
